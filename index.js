5e7ce51982b62c1ba03f85267974c4f8bcbfef3a
Eljah Capers <elicasper2000@yahoo.com>
2024-02-07 00:37:25 -0500

    Completed Lab

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Code your solutions in this file
 for (let age = 30; age < 40; age++){
     console.log(`I'm ${age} years old. Happy birthday to me!`);
+    
 }
 
 const gifts = ["teddy bear", "drone", "doll"];
@@ -8,7 +9,6 @@ const gifts = ["teddy bear", "drone", "doll"];
 function wrapGifts(gifts){
     for (let i = 0; i < gifts.length; i++){
         console.log(`Wrapped ${gifts[i]} and added a bow!`);
-        debugger;
     }
     return gifts;
 }
